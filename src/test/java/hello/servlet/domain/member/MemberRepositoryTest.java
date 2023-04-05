899d4b7973846f1eae137714575d493b6de24c56
jelly1010p <jelly1010p@gmail.com>
2023-04-06 07:44:17 +0900

    servlet + jsp의 시작

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,56 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance(); //싱글톤이므로 new 사용 안한다
+
+    //test끝난 후 클리어
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member saveMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember); //찾은 멤버랑 저장멤버랑 같아야함
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("mem1", 20);
+        Member member2 = new Member("mem2", 30);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1,member2);
+
+
+    }
+
+
+
+
+}
